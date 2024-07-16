a753245346c941d7740fcc5249d553ebeba4a57a
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-07-16 23:50:29 +0900

    :recycle: [Refactor]: request dto에 기본생성자 어노테이션 추가

--- src/main/java/timify/com/study/dto/StudyRequest.java
+++ src/main/java/timify/com/study/dto/StudyRequest.java
@@ -3,9 +3,11 @@ package timify.com.study.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 public class StudyRequest {
     @Getter
+    @NoArgsConstructor
     public static class studyTypeRequest {
         @NotBlank
         @Size(min = 1, max = 30)
@@ -13,6 +15,7 @@ public class StudyRequest {
     }
 
     @Getter
+    @NoArgsConstructor
     public static class studyMethodRequest {
         @NotBlank
         @Size(min = 1, max = 30)
@@ -20,6 +23,7 @@ public class StudyRequest {
     }
 
     @Getter
+    @NoArgsConstructor
     public static class studyPlaceRequest {
         @NotBlank
         @Size(min = 1, max = 30)
