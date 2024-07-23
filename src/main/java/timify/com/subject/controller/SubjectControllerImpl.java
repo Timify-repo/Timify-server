d4e6e62c8b5e9f81a039294069c0b86127bb256c
jaehyuk <bjh010321@naver.com>
2024-07-23 21:33:48 +0900

    :recycle: [Refactor]: @Override 어노테이션 추가

--- src/main/java/timify/com/subject/controller/SubjectControllerImpl.java
+++ src/main/java/timify/com/subject/controller/SubjectControllerImpl.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -32,6 +33,7 @@ public class SubjectControllerImpl implements SubjectController {
 
     private final SubjectService subjectService;
 
+    @Override
     @PostMapping("/insert")
     public ApiResponse<subjectInfoDto> registerSubject(
         @AuthMember Member member,
@@ -41,6 +43,7 @@ public class SubjectControllerImpl implements SubjectController {
         return ApiResponse.onSuccess(subjectDto);
     }
 
+    @Override
     @GetMapping()
     public ApiResponse<getListDto> activeSubjectAll(@AuthMember Member member,
         @RequestParam("status") String status) {
@@ -49,6 +52,7 @@ public class SubjectControllerImpl implements SubjectController {
         return ApiResponse.onSuccess(activeSubjectList);
     }
 
+    @Override
     @DeleteMapping("/delete/{subjectId}")
     public ApiResponse<Long> deleteSubject(@AuthMember Member member,
         @PathVariable(name = "subjectId") Long subjectId) {
@@ -57,7 +61,8 @@ public class SubjectControllerImpl implements SubjectController {
         return ApiResponse.of(SuccessStatus.SUBJECT_DELETE_SUCCESS, deleteId);
     }
 
-    @PutMapping("/order/{subjectId}/{orderNum}")
+    @Override
+    @PatchMapping("/order/{subjectId}/{orderNum}")
     public ApiResponse<updateOrderNumDto> changeOrder(@AuthMember Member member,
         @PathVariable(name = "subjectId") Long subjectId,
         @PathVariable int orderNum) {
@@ -67,7 +72,8 @@ public class SubjectControllerImpl implements SubjectController {
         return ApiResponse.of(SuccessStatus.ORDER_CHANGE_SUCCESS, updateOrderNumDto);
     }
 
-    @PutMapping("/insert/{subjectId}")
+    @Override
+    @PatchMapping("/insert/{subjectId}")
     public ApiResponse<updateTitleNameDto> updateTitle(@AuthMember Member member,
         @RequestBody @Valid subjectRequest request,
         @PathVariable(name = "subjectId") Long subjectId) {
@@ -77,7 +83,8 @@ public class SubjectControllerImpl implements SubjectController {
         return ApiResponse.of(SuccessStatus.TITLE_CHANGE_SUCCESS, updateTitleNameDto);
     }
 
-    @PutMapping("/{subjectId}/update-status")
+    @Override
+    @PatchMapping("/{subjectId}/update-status")
     public ApiResponse<Long> updateStatus(@AuthMember Member member,
         @RequestParam(name = "status") String status,
         @PathVariable(name = "subjectId") Long subjectId) {
