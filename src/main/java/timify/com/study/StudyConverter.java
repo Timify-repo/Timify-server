73848bcdfaef528ed2fc5363055c7b4ebacc9557
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-08-19 19:49:32 +0900

    :bug: [Fix] converter에 isDefault 추가

--- src/main/java/timify/com/study/StudyConverter.java
+++ src/main/java/timify/com/study/StudyConverter.java
@@ -7,51 +7,55 @@ import timify.com.study.domain.StudyType;
 import timify.com.study.dto.StudyResponse;
 
 public class StudyConverter {
+
     public static StudyType toStudyType(String title, int order) {
         return StudyType.builder()
-                .title(title)
-                .orderNum(order)
-                .status(CategoryStatus.ACTIVE)
-                .build();
+            .title(title)
+            .orderNum(order)
+            .status(CategoryStatus.ACTIVE)
+            .isDefault(false)
+            .build();
     }
 
     public static StudyResponse.studyTypeDto toStudyTypeDto(StudyType studyType) {
         return StudyResponse.studyTypeDto.builder()
-                .studyTypeId(studyType.getId())
-                .order(studyType.getOrderNum())
-                .studyTypeTitle(studyType.getTitle())
-                .build();
+            .studyTypeId(studyType.getId())
+            .order(studyType.getOrderNum())
+            .studyTypeTitle(studyType.getTitle())
+            .build();
     }
 
     public static StudyMethod toStudyMethod(String title, int order) {
         return StudyMethod.builder()
-                .title(title)
-                .orderNum(order)
-                .status(CategoryStatus.ACTIVE)
-                .build();
+            .title(title)
+            .orderNum(order)
+            .status(CategoryStatus.ACTIVE)
+            .isDefault(false)
+            .build();
     }
 
     public static StudyResponse.studyMethodDto toStudyMethodDto(StudyMethod studyMethod) {
         return StudyResponse.studyMethodDto.builder()
-                .studyMethodId(studyMethod.getId())
-                .order(studyMethod.getOrderNum())
-                .studyMethodTitle(studyMethod.getTitle())
-                .build();
+            .studyMethodId(studyMethod.getId())
+            .order(studyMethod.getOrderNum())
+            .studyMethodTitle(studyMethod.getTitle())
+            .build();
     }
 
     public static StudyPlace toStudyPlace(String title, int order) {
         return StudyPlace.builder()
-                .title(title)
-                .orderNum(order)
-                .status(CategoryStatus.ACTIVE)
-                .build();
+            .title(title)
+            .orderNum(order)
+            .status(CategoryStatus.ACTIVE)
+            .isDefault(false)
+            .build();
     }
 
     public static StudyResponse.studyPlaceDto toStudyPlaceDto(StudyPlace studyPlace) {
         return StudyResponse.studyPlaceDto.builder()
-                .studyPlaceId(studyPlace.getId())
-                .order(studyPlace.getOrderNum())
-                .studyPlaceTitle(studyPlace.getTitle())
-                .build();
+            .studyPlaceId(studyPlace.getId())
+            .order(studyPlace.getOrderNum())
+            .studyPlaceTitle(studyPlace.getTitle())
+            .build();
     }
 }
