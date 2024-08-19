e9e2fdf7d73f9a9363ecfea8b29ac46380c5b134
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-08-19 17:02:54 +0900

    :recycle: [Refactor] StudyType, StudyMethod, StudyPlace 엔티티에 isDefault 추가

--- src/main/java/timify/com/study/domain/StudyType.java
+++ src/main/java/timify/com/study/domain/StudyType.java
@@ -1,7 +1,21 @@
 package timify.com.study.domain;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import timify.com.domain.common.BaseDateTimeEntity;
 import timify.com.member.domain.Member;
 
@@ -11,6 +25,7 @@ import timify.com.member.domain.Member;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class StudyType extends BaseDateTimeEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "study_type_id")
@@ -22,6 +37,9 @@ public class StudyType extends BaseDateTimeEntity {
 
     @Column(nullable = false, length = 3)
     private int orderNum;
+
+    @Column(nullable = false)
+    private boolean isDefault;
 
     @Enumerated(EnumType.STRING)
     @Column(columnDefinition = "VARCHAR(10)", nullable = false)
